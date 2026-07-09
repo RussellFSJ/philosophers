a6c484b16849b7386ea70eafc9ff3224fd7b1a71
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-07-09 16:12:08 +0800

    fix: change error message from integer arguments to numeric arguments to accomodate for long arguments

--- philo/src/args/valid_args.c
+++ philo/src/args/valid_args.c
@@ -6,7 +6,7 @@
 /*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 19:49:10 by rfoo              #+#    #+#             */
-/*   Updated: 2026/06/22 18:05:30 by rfoo             ###   ########.fr       */
+/*   Updated: 2026/07/09 16:11:04 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ int	valid_args(int argc, char **argv)
 {
 	if (argc != 5 && argc != 6)
 	{
-		printf("Error: Please enter 4 or 5 integer arguments.\n");
+		printf("Error: Please enter 4 or 5 numeric arguments.\n");
 		print_usage();
 		return (0);
 	}
 	if (!ft_isnbr(argv[1]) || !ft_isnbr(argv[2])
 		|| !ft_isnbr(argv[3]) || !ft_isnbr(argv[4]))
 	{
-		printf("Error: Please provide integer arguments.\n");
+		printf("Error: Please provide numeric arguments.\n");
 		print_usage();
 		return (0);
 	}
